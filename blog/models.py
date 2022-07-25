62192484e96f54dca98d55710e09fb41c52cef13
Daniar <daniyaruteg123@gmail.com>
2022-07-25 13:54:49 +0500

    tests done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from statistics import mode
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        #here we need create a new instance of model
+        self.blog = Post.objects.create(title = 'django', author='dan', slug='dj')
+
+
+    def test_post_model(self):
+
+        d = self.blog
+        self.assertTrue(d.title == 'django')
+        self.assertTrue(d.slug == 'dj')
+        self.assertTrue(d.author == 'dan')
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d.author), 'dan')
+        self.assertEqual(str(d), 'django')
+        
